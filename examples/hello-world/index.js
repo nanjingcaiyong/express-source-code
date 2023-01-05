9f20a1c61250db1f10257c9c7481a148a9cfaf17
caiyong <yong.cai@kapeixi.com>
2023-01-05 13:01:46 +0800

    feat:

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -4,12 +4,22 @@ var express = require('../../');
 
 var app = module.exports = express()
 
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
+// app.get('/', function(req, res){
+//   console.log('start', new Date())
+//   setTimeout(function () {
+//     console.log('end', new Date())
+//     res.send('Hello World');
+//   }, 10000);
+// });
+
+app.route('/')
+  .get(function (req, res) {
+    
+    res.send('hello world')
+  })
 
 /* istanbul ignore next */
 if (!module.parent) {
-  app.listen(3000);
+  app.listen(3001);
   console.log('Express started on port 3000');
 }
